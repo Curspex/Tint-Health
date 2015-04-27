c3eb05fe2336ccf85c06b26fed23166bada5d136
8rrico <8rrico@gmail.com>
2015-04-27 11:52:29 -0400

    Cleanup

--- src/fr/mrsheepsheep/tinthealth/TintHealth.java
+++ src/fr/mrsheepsheep/tinthealth/TintHealth.java
@@ -1,16 +1,15 @@
 package fr.mrsheepsheep.tinthealth;
 
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitScheduler;
 
-public class TintHealth extends JavaPlugin implements Listener {
+public class TintHealth extends JavaPlugin{
 
 	protected TintHealth plugin;
 	protected THFunctions functions;
 	protected boolean fade = false;
-	protected int fadetime;
+	protected int fadetime = 5;
 	protected int intensity = 1;
 
 	public void onEnable(){
@@ -20,15 +19,15 @@ public class TintHealth extends JavaPlugin implements Listener {
 		FileConfiguration config = getConfig();
 		config.options().copyDefaults(true);
 
-		config.addDefault("options.fade-enabled", false);
-		config.addDefault("options.fade-time", 5);
-		config.addDefault("options.intensity-modifier", 1);
+		config.addDefault("options.fade-enabled", this.fade);
+		config.addDefault("options.fade-time", this.fadetime);
+		config.addDefault("options.intensity-modifier", this.intensity);
 
 		saveConfig();
 
-		fade = config.getBoolean("options.fade-enabled");
-		fadetime = config.getInt("options.fade-time");
-		intensity = config.getInt("options.intensity-modifier");
+		this.fade = config.getBoolean("options.fade-enabled");
+		this.fadetime = config.getInt("options.fade-time");
+		this.intensity = config.getInt("options.intensity-modifier");
 
 	}
 	
